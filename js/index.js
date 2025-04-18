73bb9f125be92efaf2d21f5769889fcc135d3bde
Melvin Escobar <melvin.escobar@macaulay.cuny.edu>
2025-04-18 17:42:51 -0400

    Navigation Functionality

--- js/index.js
+++ js/index.js
@@ -2,11 +2,24 @@
 fetch('../html/navigation.html')
   .then(res => res.text())
   .then(html => {
-    document.getElementById('navigation-sect').innerHTML = html;
+    document.getElementById('navigation-section').innerHTML = html;
+
+    // After navigation is loaded attach listeners
+    document.querySelectorAll('.nav-btn').forEach(btn => {
+      btn.addEventListener('click', () => {
+        const page = btn.getAttribute('data-page');
+        fetch('../html/' + page)
+          .then(res => res.text())
+          .then(html => {
+            document.getElementById('content-section').innerHTML = html;
+          });
+      });
+    });
 });
 
+// Load the default page (homepage)
 fetch('../html/homepage.html')
   .then(res => res.text())
   .then(html => {
-    document.getElementById('homepage-sect').innerHTML = html;
+    document.getElementById('content-section').innerHTML = html;
 });
