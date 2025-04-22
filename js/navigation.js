cc8f547d1f778a795e2c584f3ce38352688ba36a
haseebmalik04 <haseebulhaq.malik29@bcmail.cuny.edu>
2025-04-21 23:42:01 -0400

    navbar shoppingcart button functionality

--- js/navigation.js
+++ js/navigation.js
@@ -1,25 +1,34 @@
 export async function loadNavigation() {
-    const res = await fetch('../html/navigation.html');
-    const html = await res.text();
-    document.getElementById('navigation-container').innerHTML = html;
-  
-    document.querySelectorAll('.nav-btn').forEach(btn => {
-      btn.addEventListener('click', async () => {
-        const page = btn.getAttribute('data-page');
-        const isSearch = btn.id === 'search-btn';
-        const searchInput = isSearch
-          ? document.getElementById('search-bar').value.toLowerCase().trim()
-          : null;
-  
-        const pageRes = await fetch('../html/' + page);
-        const pageHtml = await pageRes.text();
-        document.getElementById('content-container').innerHTML = pageHtml;
-  
-        if (page === 'results.html') {
-          const module = await import('../js/results.js');
-          module.loadResults(searchInput);
-        }
-      });
+  const res = await fetch("../html/navigation.html");
+  const html = await res.text();
+  document.getElementById("navigation-container").innerHTML = html;
+
+  document.querySelectorAll(".nav-btn").forEach((btn) => {
+    btn.addEventListener("click", async (event) => {
+      if (btn.tagName === "A") {
+        event.preventDefault();
+      }
+
+      const page = btn.getAttribute("data-page");
+
+      if (page === "shoppingcart.html") {
+        window.location.href = "../html/shoppingcart.html";
+        return;
+      }
+
+      const isSearch = btn.id === "search-btn";
+      const searchInput = isSearch
+        ? document.getElementById("search-bar").value.toLowerCase().trim()
+        : null;
+
+      const pageRes = await fetch("../html/" + page);
+      const pageHtml = await pageRes.text();
+      document.getElementById("content-container").innerHTML = pageHtml;
+
+      if (page === "results.html") {
+        const module = await import("../js/results.js");
+        module.loadResults(searchInput);
+      }
     });
-  }
-  
+  });
+}
